b855bb18b38d821a8b262928de7402f0b54dba08
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-17 01:07:01 -0400

    Grid responsive usando break points

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     {{-- <div class="container mx-auto"> --}}
     <div class="container">
         {{-- Grid de tailwind --}}
-        <div class="grid grid-cols-4 gap-2">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             <div class="bg-blue-200">A</div>
             <div class="bg-blue-300">B</div>
             <div class="bg-blue-400">C</div>
